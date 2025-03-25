25bb3bf19990dc3faa9df0dbb2c62ed2fe55e356
신재광[플레이스검색서비스개발] <jaekwang.shin@navercorp.com>
2025-03-25 14:50:44 +0900

    chore: ESLint 및 TypeScript 설정 개선
    - ESLint 설정 개선: 빌드 디렉토리 무시 설정 추가, Node.js 및 브라우저 전역 변수 설정 추가, JavaScript 파일 설정 추가
    
    - TypeScript 설정 정리: 불필요한 주석 제거, paths 설정 위치 조정

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,12 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/dist-electron/**', '**/release/**', '**/node_modules/**']
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ['dist', 'dist-electron'],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       parserOptions: {
@@ -18,12 +20,15 @@ export default tseslint.config(
           jsx: true
         }
       },
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        process: true,
+        __dirname: true
+      }
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       'no-unused-vars': 'off',
@@ -39,8 +44,22 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
-      ],
-    },
+        { allowConstantExport: true }
+      ]
+    }
   },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        process: true,
+        __dirname: true,
+        module: true,
+        require: true
+      }
+    }
+  }
 )
