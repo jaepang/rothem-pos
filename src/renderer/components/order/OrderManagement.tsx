657172bbd98a48ad5511220a6d0265394a7873bc
신재광[플레이스검색서비스개발] <jaekwang.shin@navercorp.com>
2025-03-27 15:08:45 +0900

    style: 메뉴 카드 레이아웃 개선 - 한 줄에 3개의 메뉴 카드가 보이도록 수정

--- src/renderer/components/order/OrderManagement.tsx
+++ src/renderer/components/order/OrderManagement.tsx
@@ -184,7 +184,7 @@ const OrderManagement: React.FC = () => {
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
         <div>
           <h3 className="text-xl font-bold mb-4">메뉴</h3>
-          <div className="grid grid-cols-2 gap-3">
+          <div className="grid grid-cols-3 gap-3">
             {displayMenus.map(menu => (
               <button
                 key={`${menu.id}-${menu.isHot}-${menu.isIce}`}
