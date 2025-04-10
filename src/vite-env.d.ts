a1428961a1a87a125f5de26d4abe9097c70b1c8a
jaepang <jaepang.shin@gmail.com>
2025-04-10 21:25:01 +0900

    chore: .gitignore에 Google API 자격증명 파일 추가 및 vite-env.d.ts에서 불필요한 ElectronAPI 인터페이스 제거

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,37 +1,4 @@
 /// <reference types="vite/client" />
 
 /// <reference path="./types/images.d.ts" />
-
-interface ElectronAPI {
-  versions: Record<string, string>;
-  path: {
-    join: (...args: string[]) => Promise<string>;
-    resolve: (...args: string[]) => Promise<string>;
-  };
-  fs: {
-    saveImage: (file: ArrayBuffer, menuId: string) => Promise<string>;
-    deleteImage: (imageUrl: string) => Promise<void>;
-  };
-  menu: {
-    loadMenuFromJson: () => Promise<any[]>;
-    saveMenuToJson: (menuList: any[]) => Promise<void>;
-  };
-  inventory: {
-    loadInventoryFromJson: () => Promise<any[]>;
-    saveInventoryToJson: (inventoryList: any[]) => Promise<void>;
-  };
-  orders: {
-    loadOrdersFromJson: () => Promise<any[]>;
-    saveOrdersToJson: (orders: any[]) => Promise<boolean>;
-  };
-  printer: {
-    initialize: () => Promise<boolean>;
-    getStatus: () => Promise<boolean>;
-    printOrder: (order: any) => Promise<void>;
-  };
-}
-
-interface Window {
-  electronAPI: ElectronAPI;
-  electron: any;
-}
+/// <reference path="./types/electron.d.ts" />
